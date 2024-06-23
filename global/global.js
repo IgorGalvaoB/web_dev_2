f5a57b25486cb5223b35e14e311e2b97c5d1a34e
Igor Galvão Bezerra <igorgalvaob@gmail.com>
2024-06-22 23:34:13 -0300

    first commit

---
+++ global/global.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function() {
+    fetch('../../global/header.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('header').innerHTML = data;
+        })
+        .catch(error => console.error('Error loading header:', error));
+});
+document.addEventListener("DOMContentLoaded", function() {
+    fetch('../../global/footer.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('footer').innerHTML = data;
+        })
+        .catch(error => console.error('Error loading header:', error));
+});
